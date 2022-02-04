f9c55025a372e5cf4524cc10aa476ce5b0f872ba
MadsenAMS <andre-madsen@hotmail.com>
2022-02-04 18:21:49 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+import laotseImg from './images/laotse.jpg';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+
+        <div >
+        <p> quote xpto - Speaker </p> 
+        <button> Quote No Jutsu </button>
+        <img src={laotseImg} alt="Statue of LaoTse Riding a Bull"></img> 
+        </div>
+
+    );
 }
 
 export default App;
